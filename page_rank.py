045c3eb0c445001644de924befce2a03f2129633
DudiChen <dudi.chen@gmail.com>
2022-01-31 00:20:33 +0200

    [DUDI] BUG FIX - corrected print for list e.g. np.where(d_current == d_current.max())) changed by adding '*' in the beginning.

--- page_rank.py
+++ page_rank.py
@@ -82,9 +82,9 @@ def incremental_iterations_page_rank(G):
     print('Final Vector = ', d_current)
     # TODO Write vector to external file
     print('Max value in d=', d_current.max())
-    print('Indices of max vale: ', np.where(d_current == d_current.max()))
+    print('Indices of max vale: ', *np.where(d_current == d_current.max()))
     print('Min value in d=', d_current.min())
-    print('Indices of min vale: ', np.where(d_current == d_current.min()))
+    print('Indices of min vale: ', *np.where(d_current == d_current.min()))
     print('Total Duration: {0} minutes'.format(total_diff_secs / 60))
 
 
