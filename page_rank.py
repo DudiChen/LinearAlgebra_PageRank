5600cc2595fdfb3d3a0fd52d0460ced9931aa744
Furanimus <babitskyd@gmail.com>
2022-01-31 02:18:52 +0200

    Added more data output

--- page_rank.py
+++ page_rank.py
@@ -7,13 +7,13 @@ import igraph
 
 NUM_OF_VERTICES_G = int(math.pow(2, 10))
 NUM_OF_VERTICES_CYCLE = int(math.pow(2, 6))
-epsilon = 0
-N = int(math.pow(2, 7))  # int(2 / epsilon)  # Path length
+epsilon = 0.5
+N = 2 / epsilon  # int(math.pow(2, 7))  # int(2 / epsilon)  # Path length
 t = 2  # default num of iterations
 p = 1 / math.pow(2, 6)  # probability for edge creation
 
 
-def page_rank(G, iterations=t):
+def page_rank(G, iterations=t, eps=epsilon):
     n = G.vcount()
     d = np.zeros(n)
 
@@ -23,7 +23,7 @@ def page_rank(G, iterations=t):
             chance_for_rand_neighbor = math.floor(rnd.uniform(0, 1))
             v0_adj_neighbors = G.neighbors(v0, mode="out")
             v0_num_adj_neighbors = len(v0_adj_neighbors)
-            if is_visit_random_neighbor(v0, v0_adj_neighbors, v0_num_adj_neighbors, chance_for_rand_neighbor):
+            if is_visit_random_neighbor(v0, v0_adj_neighbors, v0_num_adj_neighbors, chance_for_rand_neighbor, eps):
                 v0_rand_neighbor_indx = math.floor(rnd.uniform(0, v0_num_adj_neighbors))
                 next_vertex = v0_adj_neighbors[v0_rand_neighbor_indx]
             else:  # We'll visit a random vertex in G (by Uniform Distribution)
@@ -35,10 +35,10 @@ def page_rank(G, iterations=t):
     return d / iterations
 
 
-def is_visit_random_neighbor(v0, v0_adj_neighbors, v0_num_adj_neighbors, chance_for_rand_neighbor):
+def is_visit_random_neighbor(v0, v0_adj_neighbors, v0_num_adj_neighbors, chance_for_rand_neighbor, eps):
     result = False
     # OPTION 1:
-    if v0_num_adj_neighbors > 0 and chance_for_rand_neighbor <= (1 - epsilon):  # Then we'll visit to a random neighbor
+    if v0_num_adj_neighbors > 0 and chance_for_rand_neighbor <= (1 - eps):  # Then we'll visit to a random neighbor
         result = True
     # TODO: QUESTION; What if v0 has only itself as a neighbor ??? Should we go to a random vertex instead?
     # OPTION 2:
@@ -55,12 +55,12 @@ def avg_node_degree(G):
     return sum(degrees) / n
 
 
-def incremental_iterations_page_rank(G):
+def incremental_iterations_page_rank(G, eps=epsilon):
     print('\nStarting incremental_iterations_page_rank...')
     time_start = datetime.datetime.now()
     iteration = 1
     t = 2
-    d_current = page_rank(G, t)
+    d_current = page_rank(G, t, eps)
     print('Completed Page-Rank of t = 2^{0} = {1}'.format(iteration, int(math.pow(2, iteration))))
     print('Finished iteration number: ', iteration)
 
@@ -80,7 +80,6 @@ def incremental_iterations_page_rank(G):
     total_diff_secs = (datetime.datetime.now() - time_start).total_seconds()
     print('Final iteration is {0}.\nresult: t = 2^{0} = {1}\n'.format(iteration, int(math.pow(2, iteration))))
     print('Final Vector = ', d_current)
-    # TODO Write vector to external file
     print('Max value in d=', d_current.max())
     print('Indices of max vale: ', *np.where(d_current == d_current.max()))
     print('Min value in d=', d_current.min())
@@ -91,11 +90,7 @@ def incremental_iterations_page_rank(G):
 def convert_nparray_to_igraph(G_nparray):
     G = igraph.Graph(directed=True)
     G.add_vertices(G_nparray.shape[0])
-    G.add_edges([(i, j) for i, row in enumerate(G_nparray)
-                 for j, _ in enumerate(row)
-                 if G_nparray[i, j] > 0])
-    # sys.stdout = open('Graph_G_test', 'w')
-    # sys.stdout.close()
+    G.add_edges([(i, j) for i, row in enumerate(G_nparray) for j, _ in enumerate(row) if G_nparray[i, j] > 0])
     return G
 
 
@@ -133,34 +128,32 @@ def create_cycle_graph_and_add_edge(G):
     return G
 
 
-# TODO: Need to clear up garbage code from 'main'
-if __name__ == '__main__':
-    NUM_OF_VERTICES_G = int(math.pow(2, 10))
-    NUM_OF_VERTICES_CYCLE = int(math.pow(2, 6))
-    epsilon = 0
-    N = int(math.pow(2, 7))  # int(2 / epsilon)  # Path length
-    t = 2  # default num of iterations
-    p = 1 / math.pow(2, 6)  # probability for edge creation
-    print('Parameters for current test:')
-    print('p=1/2^{0}, epsilon={1}, N=2^{2}\n'.format(-math.log(p, 2), epsilon, (math.log(N, 2))))
-    print('Parameters for Graph creation:')
-    print('NUM_OF_VERTICES_G=2^{0}, NUM_OF_VERTICES_CYCLE=2^{1}\n'.format(math.log(NUM_OF_VERTICES_G, 2),
-                                                                          math.log(NUM_OF_VERTICES_CYCLE, 2)))
-    G = create_random_igraph_with_probability()
-    G = create_cycle_graph_and_add_edge(G)
-    print('Average degree: ', avg_node_degree(G))
-    # d = page_rank(G)
-    incremental_iterations_page_rank(G)
-    print('------------------------------------')
+def incremental_epsilon_test(G):
+    eps = epsilon
+    print('Running incremental Epsilon page_ranks...')
+    while eps >= 1 / math.pow(2, 10):
+        print('Current epsilon=1/2^{0}'.format(-math.log(eps, 2)))
+        incremental_iterations_page_rank(G, eps)
+        print('Finished iteration for epsilon=1/2^{0}'.format(-math.log(eps, 2)))
+        eps *= 0.5
+        print('********************************************')
 
-# G = nx.DiGraph(
-#     np.array([
-#         [1, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 1, 0, 0, 1, 0],
-#         [1, 0, 1, 0, 0, 0, 0],
-#         [1, 0, 1, 0, 1, 1, 1],
-#         [0, 1, 0, 0, 0, 0, 1],
-#         [0, 0, 1, 0, 0, 1, 1],
-#         [0, 0, 0, 1, 1, 0, 1]
-#     ])
-# )
+    # TODO: Need to clear up garbage code from 'main'
+if __name__ == '__main__':
+        NUM_OF_VERTICES_G = int(math.pow(2, 10))
+        NUM_OF_VERTICES_CYCLE = int(math.pow(2, 6))
+        N = int(math.pow(2, 7))  # int(2 / epsilon)  # Path length
+        t = 2  # default num of iterations
+        p = 1 / math.pow(2, 6)  # probability for edge creation
+        print('Parameters for current test:')
+        print('p=1/2^{0}, epsilon={1}, N=2^{2}\n'.format(-math.log(p, 2), epsilon, (math.log(N, 2))))
+        print('Parameters for Graph creation:')
+        print('NUM_OF_VERTICES_G=2^{0}, NUM_OF_VERTICES_CYCLE=2^{1}\n'.format(math.log(NUM_OF_VERTICES_G, 2),
+                                                                              math.log(NUM_OF_VERTICES_CYCLE, 2)))
+        G = create_random_igraph_with_probability()
+        G = create_cycle_graph_and_add_edge(G)
+        print('Average degree: ', avg_node_degree(G))
+        # d = page_rank(G)
+        # incremental_iterations_page_rank(G)
+        incremental_epsilon_test(G)
+        print('------------------------------------')
