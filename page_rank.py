3cdf5ec96692b5d8ed6e288178aed91339744870
Furanimus <babitskyd@gmail.com>
2022-02-01 04:33:20 +0200

    some adjustments to write_to_file

--- page_rank.py
+++ page_rank.py
@@ -1,4 +1,4 @@
-import sys
+import statistics
 import numpy as np
 import random as rnd
 import math
@@ -7,9 +7,9 @@ import igraph
 
 NUM_OF_VERTICES_G = int(math.pow(2, 10))
 NUM_OF_VERTICES_CYCLE = int(math.pow(2, 6))
-epsilon = 0.5
+epsilon = 0
 file_index = 1
-N = int(2 / epsilon)  # int(math.pow(2, 10)) # Path length
+N = int(math.pow(2, 7))  # int(2 / epsilon)  # int(math.pow(2, 6)) # Path length
 t = 2  # default num of iterations
 p = 1 / math.pow(2, 6)  # probability for edge creation
 
@@ -33,15 +33,13 @@ def page_rank(G, iterations=t, eps=epsilon):
         d[next_vertex] += 1
 
     normalized_d = d / iterations
-    write_vector_to_file(normalized_d, eps)
-    print(normalized_d)
     return normalized_d
 
 
 def write_vector_to_file(vector, eps):
     global file_index
-    f = open('Final_vector_d_{0}_eps_2_{1}'.format(file_index,-math.log(eps, 2)), 'w')
-    f.write('Epsilon=2^{0}\n\n'.format(-math.log(eps, 2)))
+    f = open('Final_vector_d_{0}'.format(file_index), 'w')
+    # f.write('Epsilon=2^{0}\n\n'.format(-math.log(eps, 2)))
     for i in range(len(vector)):
         f.write('[{0}]={1} | '.format(i, vector[i]))
     f.write('\n\nAverage of normalized_d={0}\n'
@@ -55,7 +53,9 @@ def write_vector_to_file(vector, eps):
                                               *np.where(vector == vector.min())))
     if len(vector) > NUM_OF_VERTICES_G:
         f.write('Average base graph distribution = {0}\n'.format(sum(vector[:NUM_OF_VERTICES_G]) / NUM_OF_VERTICES_G))
-        f.write('Average cycle graph distribution = {0}'.format(sum(vector[NUM_OF_VERTICES_G:]) / NUM_OF_VERTICES_CYCLE))
+        f.write('Average cycle graph distribution = {0}\n'.format(sum(vector[NUM_OF_VERTICES_G:]) / NUM_OF_VERTICES_CYCLE))
+        f.write('Median of base graph ={0} \n'.format(statistics.median(vector[:NUM_OF_VERTICES_G])))
+        f.write('Median of Cycle graph ={0} \n'.format(statistics.median(vector[NUM_OF_VERTICES_G:])))
     f.close()
     file_index += 1
 
@@ -88,7 +88,7 @@ def incremental_iterations_page_rank(G, eps=epsilon):
         iteration += 1
         t *= 2
         d_previous = d_current
-        d_current = page_rank(G, t)
+        d_current = page_rank(G, t, eps)
         dist_len = np.linalg.norm(d_current - d_previous)
         print('Completed Page-Rank of t = 2^{0} = {1}'.format(iteration, int(math.pow(2, iteration))))
         print('||v1 - v0|| = ', dist_len)
@@ -98,6 +98,7 @@ def incremental_iterations_page_rank(G, eps=epsilon):
             break
 
     total_diff_secs = (datetime.datetime.now() - time_start).total_seconds()
+    write_vector_to_file(d_current, eps)
     print('Final iteration is {0}.\nresult: t = 2^{0} = {1}\n'.format(iteration, int(math.pow(2, iteration))))
     print('Final Vector = ', d_current)
     print('Max value in d=', d_current.max())
@@ -149,9 +150,11 @@ def create_cycle_graph_and_add_edge(G):
 
 
 def incremental_epsilon_test(G):
+    global N
     eps = epsilon
     print('Running incremental Epsilon page_ranks...')
     while eps >= 1 / math.pow(2, 10):
+        N = int(2/eps)
         print('Current epsilon=1/2^{0}'.format(-math.log(eps, 2)))
         incremental_iterations_page_rank(G, eps)
         print('Finished iteration for epsilon=1/2^{0}'.format(-math.log(eps, 2)))
@@ -166,10 +169,10 @@ if __name__ == '__main__':
     print('Parameters for Graph creation:')
     print('NUM_OF_VERTICES_G=2^{0}, NUM_OF_VERTICES_CYCLE=2^{1}\n'.format(math.log(NUM_OF_VERTICES_G, 2),
                                                                           math.log(NUM_OF_VERTICES_CYCLE, 2)))
-    G = create_random_igraph_with_probability()
-    G = create_cycle_graph_and_add_edge(G)
+    G = create_random_igraph_with_probability(True)
+    # G = create_cycle_graph_and_add_edge(G)
     print('Average degree: ', avg_node_degree(G))
     # d = page_rank(G)
-    # incremental_iterations_page_rank(G)
-    incremental_epsilon_test(G)
+    incremental_iterations_page_rank(G)
+    # incremental_epsilon_test(G)
     print('------------------------------------')
